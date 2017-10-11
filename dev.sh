f87c9972d8077f45b3643f0cbecab4e7d29da62d
Tácio Tavares <taciogt@users.noreply.github.com>
2017-10-10 21:55:57 -0300

    improves dev.sh functions

--- dev.sh
+++ dev.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Inspirado no dev.sh do https://github.com/tonylampada/djangular3
+# Inspired by dev.sh from https://github.com/tonylampada/djangular3
 
 # set +e
 
@@ -10,14 +10,6 @@ GREEN='\033[00;32m'
 YELLOW='\033[00;33m'
 BLUE='\[\e[0;34m\]'
 
-# Pq ninguem merece ter que ficar decorando comando
-# Instruções:
-# 1) ". dev.sh"
-# 2) "devhelp"
-# 3) Seja feliz
-
-# workon <venv>  # Muda isso pro nome do virtalenv do seu projeto
-
 export PROJ_BASE="$(dirname ${BASH_SOURCE[0]})"
 CD=$(pwd)
 cd $PROJ_BASE
@@ -25,16 +17,17 @@ export PROJ_BASE=$(pwd)
 cd $CD
 
 function devhelp {
-    echo_green "Dica: autocomplete funciona pros comandos abaixo ;)"
+    echo_green "Tip: Autocomplete works for the commands below ;)"
     echo_red   "------------------------------------------------------------------------"
-    echo -e "${GREEN}devhelp${RESTORE}             Imprime este ${RED}help${RESTORE}"
+    echo -e "${GREEN}devhelp${RESTORE}             Prints this ${RED}help${RESTORE}"
     echo -e ""
-    echo -e "${GREEN}gitstats${RESTORE}            Faz um git fetch + git status. Util pra evitar surpresas"
-    echo -e "                    antes de um push."
+    echo -e "${GREEN}gitstats${RESTORE}            Executes a git fetch and git status."
     echo -e ""
-    echo -e "${GREEN}runserver${RESTORE}           Runs the Django Webserver for development"
+    echo -e "${GREEN}runserver${RESTORE}           Runs the Django's server for development"
     echo -e ""
-    echo -e "${GREEN}rebuild_venv${RESTORE}            Recria o virtualenv do Viva Decora"
+    echo -e "${GREEN}rebuild_venv${RESTORE}        Clear the virtualenv and reinstall the project dependencies."
+    echo -e ""
+    echo -e "${GREEN}produce_alias${RESTORE}       Shows how to create an alias to the development environment."
     echo -e ""
 }
 
@@ -53,10 +46,8 @@ function runserver(){
 }
 
 function rebuild_venv(){
-    rm -rf ${PROJ_BASE}/venv/
-    mkdir venv
-    virtualenv -p /usr/local/bin/python3 ${PROJ_BASE}/venv/
-    source ${PROJ_BASE}/venv/bin/activate
+    wipeenv homologme
+    workon homologme
     pip install --upgrade pip
     pip install -r requirements.txt
     find . -name \*.pyc -delete
@@ -64,16 +55,24 @@ function rebuild_venv(){
 
 function produce_alias {
     echo "------------------------------------------------------------------------"
-    echo "Este comando verde cria um alias que voce pode usar para cair"
-    echo "no ambdev deste projeto a partir de qualquer lugar do seu bash."
-    echo "Sugestão: adiciona no seu ~/.bashrc (Linux) ou no ~/.bash_profile (OS X)"
+    echo "This green command creates an alias that can be used to get in"
+    echo "the development environment from anywhere in your bash."
+    echo "Sugestion: insert this in your ~/.bashrc (Linux) or ~/.bash_profile (OS X)"
     echo "------------------------------------------------------------------------"
     echo_green "f_homologue-me() {"
-    echo_green "   cd ~/projects/homologue-me"
-    echo_green "   source venv/bin/activate"
+    echo_green "   cd $(pwd)"
+    echo_green "   workon homologme"
     echo_green "   . dev.sh"
     echo_green "   devhelp"
     echo_green "}"
     echo_green "alias homologme=f_homologue-me"
     echo "------------------------------------------------------------------------"
+}
+
+function echo_green {
+    echo -e "${GREEN}$1${RESTORE}";
+}
+
+function echo_red {
+    echo -e "${RED}$1${RESTORE}";
 }
